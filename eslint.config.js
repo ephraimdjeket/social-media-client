4c543fc0ddd17b6a0b812d0cf439246b2e9dfe8e
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-10-03 18:54:32 +0200

    Fixed ESLint issues

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,36 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    rules: {},
+  },
+  {
+    files: ["**/*.test.js"],
+    plugins: {
+      jest: {
+        rules: {
+          "jest/prefer-expect-assertions": "off",
+        },
+      },
+    },
+    languageOptions: {
+      globals: {
+        jest: "readonly",
+        global: "readonly",
+      },
+    },
+    extends: ["plugin:jest/recommended"],
+  },
 ];
