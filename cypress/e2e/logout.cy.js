00411ad7472a5984af5ce9f9ef954ad50b39db0b
Ephraim Djeket <ephdje50146@stud.noroff.no>
2024-10-15 12:15:42 +0200

    Edited logout unit test

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,34 +1,48 @@
-describe("logout", () => {
+describe("Logout function", () => {
   beforeEach(() => {
     cy.visit("/");
     cy.clearAllLocalStorage();
-    cy.wait(1000);
-    cy.get("form[id='registerForm']").within(() => {
-      cy.get("button[data-bs-target='#loginModal']:visible").click();
-    });
-  });
 
-  it("logs in with the user and then logs out", () => {
-    cy.wait(1200);
+    cy.wait(1000);
+
+    cy.get("form[id='registerForm']").within(() => {
+      cy.get("button[data-bs-target='#loginModal']:visible").click({
+        multiple: true,
+      });
+    });
+
+    cy.wait(1100);
+
     cy.get("#loginModal #loginEmail").should("exist");
     cy.get("#loginModal #loginPassword").should("exist");
+
     cy.get("#loginModal #loginEmail").type("ephdje50147@stud.noroff.no");
-    cy.wait(600);
+    cy.wait(400);
     cy.get("#loginModal #loginPassword").type("eph12345678");
-    cy.wait(600);
-    cy.get("button[type='submit']:visible").click();
+    cy.wait(400);
+    cy.get("button[type='submit']:visible").click({ multiple: true });
+
+    cy.wait(2100);
+  });
+
+  it("should log in and then log out successfully", () => {
+    cy.window().then((win) => {
+      const profile = win.localStorage.getItem("profile");
+      const token = win.localStorage.getItem("token");
+
+      expect(profile).to.not.be.null;
+      expect(token).to.not.be.null;
+    });
+
+    cy.get("button[data-auth='logout']:visible").click();
+
     cy.wait(1000);
 
     cy.window().then((win) => {
-      expect(win.localStorage.getItem("profile")).to.not.be.null;
-      expect(win.localStorage.getItem("token")).to.not.be.null;
-
-      cy.get("button[data-auth='logout']:visible").click();
-
-      cy.window().then((win) => {
-        expect(win.localStorage.getItem("profile")).to.be.null;
-        expect(win.localStorage.getItem("token")).to.be.null;
-      });
+      expect(win.localStorage.getItem("profile")).to.be.null;
+      expect(win.localStorage.getItem("token")).to.be.null;
     });
+
+    cy.url().should("include", "/");
   });
 });
